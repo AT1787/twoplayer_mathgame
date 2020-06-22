e0f83d34872cec9d598d97e10d6349019c527337
Andrew Trac <andrewtrac87@hotmail.com>
2020-06-22 21:25:46 +0000

    Debugged the while loop and initialize method

--- main.rb
+++ main.rb
@@ -1,43 +1,52 @@
 require './players'
 require './questions'
 
-
-
-
 class Game
     attr_accessor :p1, :p2, :current_player
 
+    def initialize()
+        @p1 = Players.new('Player 1')
+        @p2 = Players.new('Player 2')
+        @current_player = @p1
+    end 
+
     def start 
-        p1 = Players.new('Player 1')
-        p2 = Players.new('Player 2')
-        current_player = p1
-    
-        while current_player.score < 3
+
+        while @p1.score < 3 && @p2.score < 3
             runGame = Questions.new()
             puts "---- NEW TURN ----"
-            print current_player.name + ", "
+            print @current_player.name + ", "
+
             if runGame.ask
                puts "YOU WIN"
-               current_player.score += 1
-               puts "P1: #{p1.score}/3 vs P2: #{p2.score}/3"
+               @current_player.score += 1
+               puts "P1: #{@p1.score}/3 vs P2: #{@p2.score}/3"
 
-               if current_player == p1
-                  current_player = p2
+               if @current_player == @p1
+                  @current_player = @p2
                else 
-                  current_player = p1
+                  @current_player = @p1
                end
 
             else 
                 puts "YOU LOSE"
-                puts "P1 #{p1.score}/3 vs P2 #{p2.score}/3"
-                if current_player == p1
-                    current_player = p2
+                puts "P1 #{@p1.score}/3 vs P2 #{@p2.score}/3"
+
+                if @current_player == @p1
+                   @current_player = @p2
                 else 
-                   current_player = p1
+                   @current_player = @p1
+                end
             end
-         end
-      end
-  end
+        end
+        
+        puts "Game Over!"
+        if @p1.score == 3
+            puts "#{@p1.name} wins!" 
+        else 
+            puts "#{@p2.name} wins!"
+        end 
+    end
 end
 
 
